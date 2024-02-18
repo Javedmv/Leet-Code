d1a3ab6224fdfe78839f63f34287865ce03f0c96
Javedmv <javedmv777@gmail.com>
2024-02-18 13:33:33 +0530

    solved 90

---
+++ 2942.js
@@ -0,0 +1,18 @@
+// 2942. Find Words Containing Character
+/**
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+var findWordsContaining = function(words, x) {
+    let indices = []
+    for(let i = 0 ; i < words.length ; i++){
+        for(let char of words[i]){
+            if(x === char){
+                indices.push(i);
+                break;
+            }
+        }
+    }
+    return indices;
+};
