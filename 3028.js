7a89da8446ce9b3e3dfb889d430cf7b8edf21e82
Javedmv <javedmv777@gmail.com>
2024-03-02 22:09:35 +0530

    array

---
+++ 3028.js
@@ -0,0 +1,14 @@
+// 3028. Ant on the Boundary
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var returnToBoundaryCount = function(nums) {
+    let count = 0, sum = 0;
+
+    for(let i=0; i<nums.length; i++) {
+        sum+=nums[i];
+        if(sum === 0) count++;
+    }
+    return count;
+};
