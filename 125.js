44eb2c8a98cfb363fb6a4370db3d2ee40458be78
Javedmv <javedmv777@gmail.com>
2024-03-14 10:32:05 +0530

    palindrome

---
+++ 125.js
@@ -0,0 +1,19 @@
+// 125. Valid Palindrome
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    if(s.length === 0)return true;
+    s = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+    let i = 0;
+    let j = s.length-1;
+    while(i < j){
+        if(s[i] != s[j]){
+            return false;
+        }
+        i++
+        j--
+    }
+    return true;
+};
