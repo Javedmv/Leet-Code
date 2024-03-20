1fa872da22361f52080ea75987bef5a8289a7cd7
Javedmv <javedmv777@gmail.com>
2024-03-20 10:07:26 +0530

    75 blind

---
+++ 121.js
@@ -0,0 +1,18 @@
+// 121. Best Time to Buy and Sell Stock
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let buy = prices[0];
+    let profit = 0;
+    for(let i = 1; i < prices.length;i++){
+        if(prices[i] < buy){
+            buy = prices[i]
+        }else if(prices[i] - buy > profit){
+            profit = prices[i] - buy;
+        }
+    }
+    return profit;
+};
+    
