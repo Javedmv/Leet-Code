df0f964d62dd92a1e0b5638952d5f6fa9956ff4b
Javedmv <javedmv777@gmail.com>
2024-03-28 11:04:25 +0530

    blind 75

---
+++ 11.js
@@ -0,0 +1,21 @@
+// 11. Container With Most Water
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length-1;
+    
+    while(left < right){
+        maxArea = Math.max(maxArea,(right-left) * Math.min(height[left],height[right]))
+        
+        if(height[left] < height[right]){
+            left++;
+        }else {
+            right--;
+        }
+    }
+    return maxArea;
+};
